01e66d16af6fa4f8b903836ec5d4b4cf26b50afa
Noah Lavine <nlavine@haverford.edu>
2008-07-28 11:56:47 -0400

    Improve build system.
    This patch changes the build system to handle SWIG modules better.
    First, the names of the extensions are changed in setup.py so that
    distutils builds correctly-named shared libraries (there's a note in
    setup.py with more information). Then, it adds a hack to copy everything
    in a build/lib* directory to a toplevel (in the working tree) directory
    called 'lib'. This puts well-named shared libraries in a consistent
    location, which means we can modify sombrero to add lib in its
    PYTHONPATH variable and not have to manually copy libraries any more.
    
    This also adds a note in the TODO file about something I noticed in the
    Python documentation, and it excludes SWIG-generated Python files from
    git (but you have to manually add them to the list).

--- setup.py
+++ setup.py
@@ -5,10 +5,26 @@
 
 from distutils.core import setup, Extension
 import distutils.ccompiler
+import distutils.dir_util # we need this for a terrible hack.
+import os                 # and we need this for the same hack.
 
 # First build the python extension modules
 
-tracemodule = Extension("Trace",
+# Note on SWIG:
+# Distutils "handles" SWIG. However, it doesn't do it very nicely. In order to
+# build a SWIG extension, you run the swig command on the .i file. For Python
+# extensions, this produces both a C file, to be compiled, and a Python file,
+# to be used as-is. Unfortunately, Distutils thinks that compiling an extension
+# produces only a C extension file, and I don't see any way to make it aware of
+# the fact that SWIG makes two. The offical method seems to be to pretend that
+# the SWIG-generated Python file was always there as part of the source, but
+# this seems like a bad idea.
+# However, in order to make this work, I'm essentially doing just that. The
+# extension modules here all have names beginning with an underscore. The
+# modules that you will want to use will *not* have names beginning with an
+# underscore, but I can't tell Distutils that right now.
+
+tracemodule = Extension("_Trace",
                         define_macros = [("FILEVERSION", "\"2.04\"")],
                         sources = ["tracer/hat-c.c", "tracer/hat-c.i"],
                         swig_opts = ["-importall", "-I/usr/include", "-I."],
@@ -19,13 +35,13 @@ tracemodule = Extension("Trace",
       #       add another SWIG option: "-D__x86_64__"
 
 # debugmodule: not used right now, but would presumably still compile a working tracer
-debugmodule = Extension("Trace_Debug",
+debugmodule = Extension("_Trace_Debug",
                         define_macros = [("FILEVERSION", "\"2.04\""), ("DEBUG", "")],
                         sources = ["hat-c.c", "hat-c.i"],
                         swig_opts = ["-importall", "-I/usr/include", "-I."],
                         include_dirs = ["."])
 
-artutilsmodule = Extension("Artutils",
+artutilsmodule = Extension("_Artutils",
                            define_macros = [("FILEVERSION", "\"2.04\""),
                                             ("VERSION", "\"2.05\"")],
                            sources = ["trace-reading/artutils.c",
@@ -35,7 +51,7 @@ artutilsmodule = Extension("Artutils",
                            swig_opts = ["-importall", "-I/usr/include", "-I."],
                            include_dirs = ["."])
 
-artmodule = Extension("Art",
+artmodule = Extension("_Art",
                       sources = ["trace-reading/art.i"],
                       swig_opts = ["-importall", "-I/usr/include", "-I."],
                       include_dirs = ["."])
@@ -45,6 +61,12 @@ setup (name = "Trace",
        version = "First Build",
        description = "This is a package to write Hat traces with Python",
        ext_modules = [tracemodule, artutilsmodule, artmodule])
+
+# XXX - HACK!
+for l in os.listdir("build"): # for everything in the "build" directory
+	if l.startswith("lib"): # if it starts with "lib"
+		# copy it to our own 'lib' directory
+		distutils.dir_util.copy_tree("build/"+l, "lib")
 
 # Then build some extra executables that aren't Python extensions
 
