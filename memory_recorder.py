c8e95843cb124afad80d069d202650724631ab0a
Noah Lavine <nlavine@haverford.edu>
2008-11-28 18:35:29 -0500

    Memory Recorder
    First draft of memory recorder finished.

--- memory_recorder.py
+++ memory_recorder.py
@@ -1,17 +1,44 @@
 class Literal(object):
-	def __init__(self, val):
+	def __init__(self, parent, val):
+		self.parent = parent
+		parent.children.append(self)
 		self.val = val
+	
+	# Program interface:
+	
+	def name():
+		return str(self.val)
+	
+	def parent():
+		return self.parent
+	
+	def children():
+		return None
+	
+	def result():
+		return None
 
 class Function(object):
-	def __init__(self, name, argnames):
+	def __init__(self, parent, name, argnames):
+		self.parent = parent
+		parent.children.append(self)
 		self.name = name
 		self.argnames = argnames
+	
+	# Data interface:
+	
+	def name():
+		return self.name
 
 class Computation(object):
-	def __init__(self, func, args):
+	def __init__(self, parent, func, args):
+		self.parent = parent
+		parent.children.append(self)
 		self.func = func
 		self.args = args
+		self.children = []
 		self.entered = False
+		self.result = None
 	
 	def enter(self):
 		self.entered = True
@@ -19,16 +46,30 @@ class Computation(object):
 	def finish(self, result):
 		self.entered = False
 		self.result = result
+	
+	# Program interface:
+	
+	def name(self):
+		return "Running " + self.func.name()
+	
+	def parent():
+		return self.parent
+	
+	def children():
+		return self.children
+	
+	def result():
+		return self.result
 
 
-def makeLiteral(val):
-	return Literal(val)
+def makeLiteral(parent, val):
+	return Literal(parent, val)
 
-def makeFunction(name, argnames):
-	return Function(name, argnames)
+def makeFunction(parent, name, argnames):
+	return Function(parent, name, argnames)
 
-def makeComputation(func, args):
-	return Computation(func, args)
+def makeComputation(parent, func, args):
+	return Computation(parent, func, args)
 
 def enterComputation(obj):
 	obj.enter()
